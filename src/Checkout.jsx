d7a32d52731541ef2fc9665f1e32695bd14fd7b7
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-18 12:12:18 +0000

    Initial commit

---
+++ src/Checkout.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+import { saveShippingAddress } from "./services/shippingService";
+
+const STATUS = {
+  IDLE: "IDLE",
+  SUBMITTED: "SUBMITTED",
+  SUBMITTING: "SUBMITTING",
+  COMPLETED: "COMPLETED",
+};
+
+// Declaring outside component to avoid recreation on each render
+const emptyAddress = {
+  city: "",
+  country: "",
+};
+
+export default function Checkout({ cart, emptyCart }) {
+  const [address, setAddress] = useState(emptyAddress);
+  const [status, setStatus] = useState(STATUS.IDLE);
+  const [saveError, setSaveError] = useState(null);
+
+  // Derived state
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
+
+  function handleChange(e) {
+    e.persist(); // persist the event
+    setAddress((curAddress) => {
+      return {
+        ...curAddress,
+        [e.target.id]: e.target.value,
+      };
+    });
+  }
+
+  function handleBlur(event) {
+    // TODO
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setStatus(STATUS.SUBMITTING);
+    if (isValid) {
+    }
+    try {
+      await saveShippingAddress(address);
+      emptyCart();
+      setStatus(STATUS.COMPLETED);
+    } catch (e) {
+      setSaveError(e);
+    }
+  }
+}
+
+  function getErrors(address) {
+    const result = {};
+    if (!address.city) result.city = "city is required";
+    if (!address.country) result.country = "Country is required";
+    return result;
+  }
+
+  if (saveError) throw saveError;
+  if (status === STATUS.COMPLETED) {
+    return <h1>Thanks for shopping!</h1>;
+  }
+
+  return (
+    <>
+      <h1>Shipping Info</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="city">City</label>
+          <br />
+          <input
+            id="city"
+            type="text"
+            value={address.city}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="country">Country</label>
+          <br />
+          <select
+            id="country"
+            value={address.country}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          >
+            <option value="">Select Country</option>
+            <option value="China">China</option>
+            <option value="India">India</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="USA">USA</option>
+          </select>
+        </div>
+
+        <div>
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Save Shipping Info"
+            disabled={status === STATUS.SUBMITTING}
+          />
+        </div>
+      </form>
+    </>
+  );
+}
